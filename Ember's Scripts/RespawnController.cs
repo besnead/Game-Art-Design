1b5433bb890c093e4835d722b55b0f104e3a3e8d
lXxMangoxXl <lXxMangoxXl@gmail.com>
2022-05-17 09:18:19 -0400

    Added the two new commented scripts <3

---
+++ Ember's Scripts/RespawnController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
+using Cursor = UnityEngine.Cursor;
+using MEC;
+
+public class RespawnController : MonoBehaviour
+{
+    private Vector3 RespawnPosition = Vector3.one + Vector3.up; //Where to save the latest Respawn location, created by the RespawnPoints
+    
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("RespawnTrigger")) //This will be the trigger underneath the map to signify the player needs to be respawned.
+        {
+            rb.position = RespawnPosition; //Set the players position back to the RespawnPoint
+            
+            //Change the velocity to zero, so that the player isn't zooming off the RespawnPoint.
+            rb.velocity = Vector3.zero;
+            rb.angularVelocity = Vector3.zero;
+        }
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("RespawnPoint")) //If you hit the collider with the tag RespawnPoint
+        {
+            //Go and save the new Respawn location in the RespawnPosition Vector3, just above the RespawnPoint object.
+            RespawnPosition = other.rigidbody.position + Vector3.up; //Make sure that you give the RespawnPoint a RigidBody!!!
+        }
+    }
+}
---
+++ Ember's Scripts/SpeedBoostScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
+using MEC;
+public class SpeedBoostScript : MonoBehaviour
+{
+    //WARNING: Make sure that your speed variable is PUBLIC, in your PlayerController script so that this script can access it correctly.
+    
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("SpeedBoost")) //On collision with the trigger "SpeedBoost"
+        {
+            other.gameObject.SetActive(false); //Disable the SpeedBoost trigger, so that it isn't re-activated.
+            Timing.RunCoroutine(OnSpeedBoost()); //And call the SpeedBoost Coroutine to start activating the speed-boost.
+        }
+    }
+
+    private IEnumerator<float> OnSpeedBoost()
+    {
+        float ogSpeed = speed; //Create a new float to back up the original speed.
+        speed += 20; //Adjust the speed you want added here, in this example, we're adding +20 speed.
+        yield return Timing.WaitForSeconds(10f); //Keep the speed-boost running for 10 seconds.
+        speed = ogSpeed; //Restore the old speed before with the backup.
+        yield return Timing.WaitForOneFrame;
+    }
+}
